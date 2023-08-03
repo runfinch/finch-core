0741eeb9ea5a5fc7393b52ef5635ef69cf42af97
Gavin Inglis <43075615+ginglis13@users.noreply.github.com>
2023-08-03 16:36:55 -0700

    fix: use --recursive and correct pathing in update rootfs (#132)
    Issue #, if available:
    
    *Description of changes:*
    
    
    *Testing done:*
    
    Ran script locally w/ `FINCH_ROOTFS_URL ?=
    https://deps.runfinch.com/common/aarch64/finch-rootfs-production-arm64-0000000000.tar.zst`,
    confirmed updated timestamp.
    
    
    - [x] I've reviewed the guidance in CONTRIBUTING.md
    
    
    #### License Acceptance
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    Signed-off-by: Gavin Inglis <giinglis@amazon.com>

--- bin/update-rootfs.sh
+++ bin/update-rootfs.sh
@@ -17,11 +17,11 @@ done
 
 [[ -z "$dependency_bucket" ]] && { echo "Error: Dependency bucket not set"; exit 1; }
 
-aarch64Deps=$(aws s3 ls s3://${dependency_bucket}/${PLATFORM}/${AARCH64} | grep "$AARCH64_FILENAME_PATTERN" | sort | tail -n 1 | awk '{print $4}')
+aarch64Deps=$(aws s3 ls s3://${dependency_bucket}/${PLATFORM}/${AARCH64}/ --recursive | grep "$AARCH64_FILENAME_PATTERN" | sort | tail -n 1 | awk '{print $4}')
 
 [[ -z "$aarch64Deps" ]] && { echo "Error: aarch64 dependency not found"; exit 1; }
 
-amd64Deps=$(aws s3 ls s3://${dependency_bucket}/${PLATFORM}/${X86_64} | grep "$AMD64_FILENAME_PATTERN" | sort | tail -n 1 | awk '{print $4}')
+amd64Deps=$(aws s3 ls s3://${dependency_bucket}/${PLATFORM}/${X86_64}/ --recursive | grep "$AMD64_FILENAME_PATTERN" | sort | tail -n 1 | awk '{print $4}')
 
 [[ -z "$amd64Deps" ]] && { echo "Error: x86_64 dependency not found"; exit 1; }
 
