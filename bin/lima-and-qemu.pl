30b04883040ada3f6ff8c6548f5e456847aeadf9
Weike Qu <weikequ@amazon.com>
2023-09-05 17:23:27 -0700

    update

--- bin/lima-and-qemu.pl
+++ bin/lima-and-qemu.pl
@@ -9,7 +9,8 @@ print "sudo may prompt for password to run FileMonitor\n";
 
 #Change this FileMonitor path for local build to installed path
 system("sudo -b /Applications/FileMonitor.app/Contents/MacOS/FileMonitor >$filemonitor 2>/dev/null");
-system("echo hi");
+system("echo marco");
 sleep(1) until -s $filemonitor;
+system("echo polo");
 
 system("sudo pkill FileMonitor");
