9ea1c6bc6432c048a8f4e882f431c2fdeb405cc7
Weike Qu <weikequ@amazon.com>
2023-09-05 17:41:21 -0700

    update

--- bin/lima-and-qemu.pl
+++ bin/lima-and-qemu.pl
@@ -43,9 +43,9 @@ $deps{$name} = "→ " . readlink($name);
 # Capture any library and datafiles access with FileMonitor
 my $filemonitor = "/tmp/filemonitor.log";
 END { system("sudo pkill FileMonitor") }
+system("sudo cat $filemonitor");
 print "sudo may prompt for password to run FileMonitor\n";
 
-system("sudo cat $filemonitor");
 #Change this FileMonitor path for local build to installed path
 system("sudo -b /Applications/FileMonitor.app/Contents/MacOS/FileMonitor >$filemonitor 2>/dev/null");
 sleep(1) until -s $filemonitor;
