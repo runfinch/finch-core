844018e0fcbe265b17cf937db7a6e398f4823a5f
Justin Alvarez <alvajus@amazon.com>
2025-10-31 12:30:17 -0400

    ci(feat): add mkosi os image building
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

---
+++ deps/mkosi/mkosi.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -x
+
+PROGRAM_NAME="$(basename "$0")"
+
+usage() {
+    cat <<EOF
+${PROGRAM_NAME} -- a simple wrapper to build multiple types of Finch images.
+
+Usage: ${PROGRAM_NAME}
+       ${PROGRAM_NAME} --arch=[x86_64|aarch64] --format=[oci|disk]
+       ${PROGRAM_NAME} -h|--help
+
+Options:
+
+  --arch: Specify the target arch for the image.
+
+  -h,--help: Print this usage message.
+EOF
+}
+
+error() {
+    printf "%s\n" "$*" >/dev/stderr
+    exit 1
+}
+
+mkosi_args=()
+
+while [ -n "${1-}" ]; do
+    case "${1}" in
+    -a | --arch)
+        arch="${2}"
+        shift
+        shift
+        ;;
+
+    --arch=*)
+        arch="${i#*=}"
+        shift
+        ;;
+
+    -h | --help)
+        usage
+        exit 0
+        ;;
+
+    --)
+        shift
+        mkosi_args=("$@")
+        break
+        ;;
+
+    -*)
+        error "Unknown option: '$1'."
+        ;;
+    esac
+done
+
+[[ -z "$arch" ]] && { echo "Error: arch not set"; exit 1; }
+
+MKOSI_OUT_DIR="./out/${arch}"
+mkdir -p "${MKOSI_OUT_DIR}"
+
+mkosi_arch=""
+case $arch in
+    x86-64)
+        mkosi_arch="x86-64"
+        ;;
+    x86_64)
+        mkosi_arch="x86-64"
+        ;;
+    amd64)
+        mkosi_arch="x86-64"
+        ;;
+    aarch64)
+        mkosi_arch="arm64"
+        ;;
+    arm64)
+        mkosi_arch="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $arch"
+        ;;
+esac
+
+# /home/fedora/mkosivenv/bin/mkosi -f --architecture="${mkosi_arch}" --output-directory="${MKOSI_OUT_DIR}" "${mkosi_args[@]}"
+# MKOSI_DNF=/usr/bin/dnf4 is needed on newer distros until this patch is availalbe https://github.com/rpm-software-management/dnf5/issues/1321
+mkosi --debug -f --architecture="${mkosi_arch}" --output-directory="${MKOSI_OUT_DIR}" "${mkosi_args[@]}"
