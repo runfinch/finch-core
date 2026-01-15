df4fcf174379e47e01d40021844184841e147f07
Swapnanil Gupta <swpnlg@amazon.com>
2026-01-15 23:42:20 +0000

    ci: extract deps versions from nerdctl-full archive
    Signed-off-by: Swapnanil Gupta <swpnlg@amazon.com>

--- bin/update-container-runtime-full-archive.sh
+++ bin/update-container-runtime-full-archive.sh
@@ -35,6 +35,26 @@ aarch64_deps_shasum=$(grep "${aarch64_deps}" "${downloaded_shasums}" | cut -d ' 
 amd64_deps="nerdctl-full-${version}-linux-amd64.tar.gz"
 amd64_deps_shasum=$(grep "${amd64_deps}" "${downloaded_shasums}" | cut -d ' ' -f 1)
 
+# Read actual dependency versions from the archive
+curr_arch=$(uname -m)
+if [[ "${curr_arch}" == "arm64" ]]; then
+  dependency_file="${aarch64_deps}"
+else
+  dependency_file="${amd64_deps}"
+fi
+curl -Lo "${PROJECT_ROOT}/downloads/${dependency_file}" "${dependency_download_url}/${dependency_file}"
+mkdir -p "${PROJECT_ROOT}/downloads/temp"
+tar -C "${PROJECT_ROOT}/downloads/temp" -xzf "${PROJECT_ROOT}/downloads/${dependency_file}"
+
+# nerdctl writes all the included components versions in this file
+nerdctl_full_readme="${PROJECT_ROOT}/downloads/temp/share/doc/nerdctl-full/README.md"
+nerdctl_version=$(grep "^- nerdctl:" "${nerdctl_full_readme}" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+containerd_version=$(grep "^- containerd:" "${nerdctl_full_readme}" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+runc_version=$(grep "^- runc:" "${nerdctl_full_readme}" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+cni_plugins_version=$(grep "^- CNI plugins:" "${nerdctl_full_readme}" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+buildkit_version=$(grep "^- BuildKit:" "${nerdctl_full_readme}" | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+rm -r "${PROJECT_ROOT}/downloads/temp" "${PROJECT_ROOT}/downloads/${dependency_file}"
+
 # Update archive file with latest artifacts and digests.
 ARCHIVE_FILE="${PROJECT_ROOT}/deps/container-runtime-full-archive.conf"
 truncate -s 0 "${ARCHIVE_FILE}"
@@ -46,4 +66,12 @@ truncate -s 0 "${ARCHIVE_FILE}"
   echo ""
   echo "X86_64_ARTIFACT=${amd64_deps}"
   echo "X86_64_256_DIGEST=${amd64_deps_shasum}"
+  echo ""
+  echo "# These versions were read from the nerdctl-full archive above."
+  echo "# PLEASE DO NOT UPDATE THESE MANUALLY"
+  echo "NERDCTL_VERSION=\"${nerdctl_version}\""
+  echo "CONTAINERD_VERSION=\"${containerd_version}\""
+  echo "BUILDKIT_VERSION=\"${buildkit_version}\""
+  echo "RUNC_VERSION=\"${runc_version}\""
+  echo "CNI_PLUGINS_VERSION=\"${cni_plugins_version}\""
 } >> "${ARCHIVE_FILE}"
