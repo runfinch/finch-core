4d9eeffb286d863ef07c25ff91aa121a86c2ea61
Swapnanil Gupta <swpnlg@amazon.com>
2026-03-19 22:04:35 +0000

    Revert "remove unnecessary al2023 packages"
    This reverts commit 04bd23cc6098fa6f3f98e68ab10543863c8b5282.

--- deps/mkosi/install-mkosi.sh
+++ deps/mkosi/install-mkosi.sh
@@ -5,7 +5,7 @@ set -eux
 git clone https://github.com/systemd/mkosi
 pushd mkosi
 git checkout 4bdb47b6dafec4f258a2dca446d67ee662dbedd4
-git apply ./../deps/mkosi/0001-add-support-for-al2023.patch
+git apply ./../deps/mkosi/0001-Add-support-for-Amazon-Linux-2023.patch
 popd
 mkdir -p "$HOME/.local/bin"
 ln -s $PWD/mkosi/bin/mkosi ~/.local/bin/mkosi
